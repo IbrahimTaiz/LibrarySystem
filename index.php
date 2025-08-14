a156a45350714767714c487d2187749345d1935b
dkh.alamri <dkh.alamri@gmail.com>
2025-08-14 08:50:49 +0300

    Create 4 Class

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+?>
+<!DOCTYPE html>
+<html lang="en" dir="rtl">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Library System</title>
+    </head>
+    <body>
+<?php include 'src/navbar.php'; ?>
+    </body>
+    <nav style="font-size: 3em; text-align: center; margin: 200px; font-weight: bold;">
+        المكتبة العربية ـ تعز
+    </nav>
+</html>
+</head>
+<body>
+   
+</body>
+</html>
--- src/Book.php
+++ src/Book.php
@@ -11,3 +11,15 @@ class Book
     }
 }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Book</title>
+</head>
+<body>
+<?php include 'src/navbar.php'; ?>
+    
+</body>
+</html>
